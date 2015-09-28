50d46231c9936a380ac03b2eeb1f615c24fecfa3
AShchennikov <anton.shchennikov@gmail.com>
2015-09-28 11:04:52 +0300

    add project calculator

---
+++ src/bsu/Calc.java
@@ -0,0 +1,26 @@
+package bsu;
+
+
+class CalcUtil {
+
+    public static double sum(double paramX,double paramY){
+        return paramX+paramY;
+    }
+
+    public static double mult(double paramX,double paramY){
+        return paramX*paramY;
+    }
+
+    public static double diff(double paramX,double paramY){
+        return paramX-paramY;
+    }
+
+    public static double dev(double paramX,double paramY){
+        if (paramY==0){
+            System.out.println("divided by zero");
+            return 0;
+        }
+        return paramX/paramY;
+    }
+
+}
